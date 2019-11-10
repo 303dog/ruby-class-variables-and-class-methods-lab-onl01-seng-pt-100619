abcfd5f29125a76888250432a4d658ae28a62f04
Paul Beckwith <303dog@gmail.com>
2019-11-10 22:09:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,35 +6,9 @@ class Song
   @@count = 0 
   @@genres = []
   
-  # def initalize(name, artist, genre)
-  #   @name = name
-  #   @artist = artist 
-  #   @genre = genre
-  #   @@count += 1 
-  #   @@genres << genre
-  # end
  
   def name
     @name = name
   end
-  
-  # def self.new(name, artist, genre)
-  #   self.new = name
-  #   self.new = artist
-  #   self.new = genre
-  # end
-  
-  # def self.count
-  #   @@count = count 
-  # end
-  
-  # def self.genres 
-  #   @genres    
-  # end
-  
-  # def genre(genres)
-  #   @genres = genres
-  # end
-    
   
 end
