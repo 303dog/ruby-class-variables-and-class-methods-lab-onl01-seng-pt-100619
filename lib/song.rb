fd77052b16a68f7d3adc6984d90aa3ef805de953
Paul Beckwith <303dog@gmail.com>
2019-11-10 22:04:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,24 +13,28 @@ class Song
     @@count += 1 
     @@genres << genre
   end
+ 
+ def name
+   name
+ end
   
-  def self.new(name, artist, genre)
-    self.new = name
-    self.new = artist
-    self.new = genre
-  end
+  # def self.new(name, artist, genre)
+  #   self.new = name
+  #   self.new = artist
+  #   self.new = genre
+  # end
   
-  def self.count
-    @@count = count 
-  end
+  # def self.count
+  #   @@count = count 
+  # end
   
-  def self.genres 
-    @genres    
-  end
+  # def self.genres 
+  #   @genres    
+  # end
   
-  def genre(genres)
-    @genres = genres
-  end
+  # def genre(genres)
+  #   @genres = genres
+  # end
     
   
 end
